8f5458d58f3043ce9e2641deb46976cc952e1f36
Hubert S <hubert.schmidt@microsoft.wsei.edu.pl>
2024-02-25 19:47:22 +0100

    Add Edit function

--- Lab 4 - notekeep/main.js
+++ Lab 4 - notekeep/main.js
@@ -1,12 +1,23 @@
 const addForm = document.getElementById('add-form');
 const notesList = document.getElementById('notes-list');
+const title = document.getElementById('title')
+const content = document.getElementById('content')
+const color = document.getElementById('color')
+let editID = null;
 
 const saveNote = (note) => {
     let notes = JSON.parse(localStorage.getItem('notes')) || [];
-    notes.push(note);
+    if(editID) {
+        const index = notes.findIndex(item => item.id === editID)
+        notes.splice(index , 1, note);
+    } else {
+        notes.push(note);
+    }
     localStorage.setItem('notes', JSON.stringify(notes));
-    renderNotes();
+    editID = null;
+    renderNotes();  
 };
+
 
 const deleteNote = (index) => {
     let notes = JSON.parse(localStorage.getItem('notes'));
@@ -14,6 +25,15 @@ const deleteNote = (index) => {
     localStorage.setItem('notes', JSON.stringify(notes));
     renderNotes();
 };
+
+const editNote = (index) => {
+    let notes = JSON.parse(localStorage.getItem('notes'))
+    let note = notes[index];
+    title.value = note.title;
+    content.value = note.content;
+    color.value = note.color;
+    editID = note.id;
+}
 
 const renderNotes = () => {
     notesList.innerHTML = '';
@@ -26,10 +46,14 @@ const renderNotes = () => {
             <h3>${note.title}</h3>
             <p>${note.content}</p>
             <p>${note.date}</p>
-            <button class="delete-btn">Usuń</button>`;
+            <button class="delete-btn">Usuń</button>
+            <button class="edit-btn">Edytuj</button>`;
         noteEl.innerHTML = noteContent;
         noteEl.querySelector('.delete-btn').addEventListener('click', () => {
             deleteNote(index);
+        });
+        noteEl.querySelector('.edit-btn').addEventListener('click', () => {
+            editNote(index);
         });
         notesList.appendChild(noteEl);
     });
@@ -37,12 +61,13 @@ const renderNotes = () => {
 
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    const id = Date.now();
     const title = addForm.querySelector('#title').value;
     const content = addForm.querySelector('#content').value;
     const color = addForm.querySelector('#color').value;
     const date = new Date().toLocaleString();
     const pinned = false;
-    saveNote({title, content, color, pinned, date});
+    saveNote({title, content, color, pinned, date, id});
     addForm.reset();
 });
 
